8a563070b5dc16861153e0d325908bfcde9b5d0c
Joshua Lin <lin2082@purdue.edu>
2026-03-01 20:13:44 -0600

    Added left leg data extraction and side-by-side comparative plotting

--- plot_results.m
+++ plot_results.m
@@ -1,114 +1,224 @@
 %% Import File
-csv_file = 'interaction_joint_data.csv';
+clear; 
+clc; 
+close all;
+csv_file_right = 'interaction_joint_data.csv';
+csv_file_left  = 'interaction_joint_data_left.csv';
 report_file = 'joint_forces_report.txt';
-if exist(csv_file, 'file') ~= 2
-    error(['File not found! Make sure ' ...
-        '    "interaction_joint_data.csv" is in folder.']);
+if exist(csv_file_right, 'file') ~= 2 || exist(csv_file_left, 'file') ~= 2
+    error('One or both CSV files not found! Run run_analysis.m first.');
 end
-data = readtable(csv_file);
-time = data.Time;
+data_R = readtable(csv_file_right);
+data_L = readtable(csv_file_left);
+time = data_R.Time;
 
-%% Function to Find Maxmimum Value
-get_peak = @(v) max(abs(v));
+%% Plot Format Setting
+set(0, 'DefaultFigureColor', 'w');      
+set(0, 'DefaultAxesColor', 'w');        
+set(0, 'DefaultAxesXColor', 'k');       
+set(0, 'DefaultAxesYColor', 'k');       
+set(0, 'DefaultAxesZColor', 'k');       
+set(0, 'DefaultTextColor', 'k');        
+set(0, 'DefaultLineLineWidth', 1.5);
 
-%% Write Report
+%% Generate Text Report
 fptr = fopen(report_file, 'w');
+
 if fptr == -1
     error('Could not open report file for writing.');
 end
 
-%Hip Joint
-fprintf(fptr, 'HIP Joint:\n');
-fprintf(fptr, '  Max Flexion Torque:    %.2f Nm\n', ...
-    get_peak(data.Moment_HipFlexion));
-fprintf(fptr, '  Max Abduction Torque:  %.2f Nm\n', ...
-    get_peak(data.Moment_HipAbduction));
-fprintf(fptr, '  Max Vertical Load:     %.2f N\n', ...
-    get_peak(data.Force_Hip_ProximoDistalForce));
-fprintf('\n');
+% Function to Find Maximum Absolute Value
+get_peak = @(v) max(abs(v));
 
-%Knee Joint
-fprintf(fptr, 'KNEE Joint:\n');
-fprintf('  Max Flexion Torque:    %.2f Nm\n', ...
-    get_peak(data.Moment_KneeFlexion));
-fprintf(fptr, '  Max Vertical Load:     %.2f N\n', ...
-    get_peak(data.Force_Knee_ProximoDistalForce));
-fprintf('\n');
+% Write Header
+fprintf(fptr, '=================================================\n');
+fprintf(fptr, '       HUMAN FORCE ANALYSIS REPORT\n');
+fprintf(fptr, '       Date: %s\n', datetime('today'));
+fprintf(fptr, '=================================================\n\n');
 
-%Ankle Joint
-fprintf(fptr, 'ANKLE Joint:\n');
-fprintf(fptr, '  Max Plantar Torque:    %.2f Nm\n', ...
-    get_peak(data.Moment_AnklePlantarFlexion));
-fprintf(fptr, '  Max Lateral Force:     %.2f N\n', ...
-    get_peak(data.Force_Ankle_MedioLateralForce));
+% HIP JOINT
+fprintf(fptr, '[HIP JOINT]\n');
+fprintf(fptr, '  * Max Flexion Torque:    Right: %6.2f Nm  |  Left: %6.2f Nm\n', ...
+    get_peak(data_R.Moment_HipFlexion), get_peak(data_L.Moment_HipFlexion));
+fprintf(fptr, '  * Max Abduction Torque:  Right: %6.2f Nm  |  Left: %6.2f Nm\n', ...
+    get_peak(data_R.Moment_HipAbduction), get_peak(data_L.Moment_HipAbduction));
+fprintf(fptr, '  * Max Vertical Load:     Right: %6.2f N   |  Left: %6.2f N\n', ...
+    get_peak(data_R.Force_Hip_ProximoDistalForce), get_peak(data_L.Force_Hip_ProximoDistalForce));
+fprintf(fptr, '  ------------------------------------------\n\n');
+
+% KNEE JOINT
+fprintf(fptr, '[KNEE JOINT]\n');
+fprintf(fptr, '  * Max Flexion Torque:    Right: %6.2f Nm  |  Left: %6.2f Nm\n', ...
+    get_peak(data_R.Moment_KneeFlexion), get_peak(data_L.Moment_KneeFlexion));
+fprintf(fptr, '  * Max Vertical Load:     Right: %6.2f N   |  Left: %6.2f N\n', ...
+    get_peak(data_R.Force_Knee_ProximoDistalForce), get_peak(data_L.Force_Knee_ProximoDistalForce));
+fprintf(fptr, '  ------------------------------------------\n\n');
+
+% ANKLE JOINT
+fprintf(fptr, '[ANKLE JOINT]\n');
+fprintf(fptr, '  * Max Plantar Torque:    Right: %6.2f Nm  |  Left: %6.2f Nm\n', ...
+    get_peak(data_R.Moment_AnklePlantarFlexion), get_peak(data_L.Moment_AnklePlantarFlexion));
+fprintf(fptr, '  * Max Lateral Force:     Right: %6.2f N   |  Left: %6.2f N\n', ...
+    get_peak(data_R.Force_Ankle_MedioLateralForce), get_peak(data_L.Force_Ankle_MedioLateralForce));
+fprintf(fptr, '  ------------------------------------------\n');
+
 fclose(fptr);
 fprintf("Report Written!\n");
 
-%% Plotting
-fig1 = figure('Name', 'Joint Torques', 'Color', 'w', 'Position', [100 100 1000 800]);
+%% FIGURE 1: HIP JOINT ANALYSIS
+fig1 = figure('Name', 'Hip Joint Analysis', 'Color', 'w', 'Position', [50 100 1200 800]);
 
-% Subplot 1: Hip
-subplot(3,1,1);
+% Right Hip Torques
+subplot(3,2,1);
 set(gca, 'XColor', 'k', 'YColor', 'k');
-plot(time, data.Moment_HipFlexion, 'Color', '#0072BD'); hold on;
-plot(time, data.Moment_HipAbduction, 'Color', '#D95319', 'LineStyle', '--');
-title('Hip Joint Moments', 'Color', 'k');
+plot(time, data_R.Moment_HipFlexion, 'Color', '#0072BD'); 
+hold on;
+plot(time, data_R.Moment_HipAbduction, 'Color', '#D95319', 'LineStyle', '--');
+title('Right Hip Torques', 'Color', 'k');
 ylabel('Torque (Nm)', 'Color', 'k'); 
 grid on;
-set(gca, 'XColor', 'k', 'YColor', 'k');
-legend('Hip Flexion', 'Hip Abduction', ...
-       'Location', 'best', 'Color', 'w', 'TextColor', 'k', 'Box', 'on');
+legend('Flexion', 'Abduction', 'Location', 'best', 'Color', 'w', 'TextColor', 'k', 'Box', 'on');
 
-% Subplot 2: Knee
-subplot(3,1,2);
+% Left Hip Torques
+subplot(3,2,2);
 set(gca, 'XColor', 'k', 'YColor', 'k');
-plot(time, data.Moment_KneeFlexion, 'Color', '#77AC30');
-title('Knee Joint Moment', 'Color', 'k');
-ylabel('Torque (Nm)', 'Color', 'k');
-grid on;
-set(gca, 'XColor', 'k', 'YColor', 'k');
-legend('Knee Flexion', 'Location', 'best', 'Color', 'w', 'TextColor', 'k');
-
-% Subplot 3: Ankle
-subplot(3,1,3);
-set(gca, 'XColor', 'k', 'YColor', 'k');
-plot(time, data.Moment_AnklePlantarFlexion, 'Color', '#A2142F');
-title('Ankle Joint Moment', 'Color', 'k');
+plot(time, data_L.Moment_HipFlexion, 'Color', '#0072BD'); 
+hold on;
+plot(time, data_L.Moment_HipAbduction, 'Color', '#D95319', 'LineStyle', '--');
+title('Left Hip Torques', 'Color', 'k');
 ylabel('Torque (Nm)', 'Color', 'k'); 
-xlabel('Time (s)', 'Color', 'k');
 grid on;
+legend('Flexion', 'Abduction', 'Location', 'best', 'Color', 'w', 'TextColor', 'k', 'Box', 'on');
+
+% Right Hip Vertical Load
+subplot(3,2,3);
 set(gca, 'XColor', 'k', 'YColor', 'k');
-legend('Ankle PlantarFlexion', 'Location', 'best', 'Color', 'w', 'TextColor', 'k');
+plot(time, abs(data_R.Force_Hip_ProximoDistalForce), 'Color', '#0072BD'); 
+title('Right Hip Vertical Load', 'Color', 'k');
+ylabel('Force (N)', 'Color', 'k'); 
+grid on;
+
+% Left Hip Vertical Load
+subplot(3,2,4);
+set(gca, 'XColor', 'k', 'YColor', 'k');
+plot(time, abs(data_L.Force_Hip_ProximoDistalForce), 'Color', '#0072BD'); 
+title('Left Hip Vertical Load', 'Color', 'k');
+ylabel('Force (N)', 'Color', 'k'); 
+grid on;
+
+% Right Hip Lateral Force
+subplot(3,2,5);
+set(gca, 'XColor', 'k', 'YColor', 'k');
+plot(time, data_R.Force_Hip_MediolateralForce, 'Color', '#D95319'); 
+title('Right Hip Lateral Force', 'Color', 'k');
+ylabel('Force (N)', 'Color', 'k'); 
+xlabel('Time (s)', 'Color', 'k'); 
+grid on;
+
+% Left Hip Lateral Force
+subplot(3,2,6);
+set(gca, 'XColor', 'k', 'YColor', 'k');
+plot(time, data_L.Force_Hip_MediolateralForce, 'Color', '#D95319'); 
+title('Left Hip Lateral Force', 'Color', 'k');
+ylabel('Force (N)', 'Color', 'k'); 
+xlabel('Time (s)', 'Color', 'k'); 
+grid on;
 
 set(fig1, 'InvertHardcopy', 'off'); 
 
-fig2 = figure('Name', 'Joint Reaction Forces', 'Color', 'w', 'Position', [150 150 1000 600]);
+%% FIGURE 2: KNEE JOINT ANALYSIS
+fig2 = figure('Name', 'Knee Joint Analysis', 'Color', 'w', 'Position', [100 150 1200 600]);
 
-% Subplot 1: Vertical Loads
-subplot(2,1,1);
+% Right Knee Torque
+subplot(2,2,1);
 set(gca, 'XColor', 'k', 'YColor', 'k');
-plot(time, abs(data.Force_Hip_ProximoDistalForce)); hold on;
-plot(time, abs(data.Force_Knee_ProximoDistalForce));
-plot(time, abs(data.Force_Ankle_ProximoDistalForce));
-title('Vertical Joint Reaction Forces', 'Color', 'k');
-ylabel('Force (N)', 'Color', 'k');
+plot(time, data_R.Moment_KneeFlexion, 'Color', '#77AC30');
+title('Right Knee Torque', 'Color', 'k');
+ylabel('Torque (Nm)', 'Color', 'k'); 
 grid on;
-set(gca, 'XColor', 'k', 'YColor', 'k');
-legend('Hip Vertical Force', 'Knee Vertical Force', 'Ankle Vertical Force', ...
-       'Location', 'best', 'Color', 'w', 'TextColor', 'k', 'Box', 'on');
 
-% Subplot 2: Lateral Loads
-subplot(2,1,2);
+% Left Knee Torque
+subplot(2,2,2);
 set(gca, 'XColor', 'k', 'YColor', 'k');
-plot(time, data.Force_Hip_MediolateralForce); hold on;
-plot(time, data.Force_Ankle_MedioLateralForce);
-title('Lateral Joint Reaction Forces', 'Color', 'k');
+plot(time, data_L.Moment_KneeFlexion, 'Color', '#77AC30');
+title('Left Knee Torque', 'Color', 'k');
+ylabel('Torque (Nm)', 'Color', 'k'); 
+grid on;
+
+% Right Knee Vertical Load
+subplot(2,2,3);
+set(gca, 'XColor', 'k', 'YColor', 'k');
+plot(time, abs(data_R.Force_Knee_ProximoDistalForce), 'Color', '#77AC30'); 
+title('Right Knee Vertical Load', 'Color', 'k');
 ylabel('Force (N)', 'Color', 'k'); 
-xlabel('Time (s)', 'Color', 'k');
+xlabel('Time (s)', 'Color', 'k'); 
 grid on;
-set(gca, 'XColor', 'k', 'YColor', 'k');
-legend('Hip Lateral Force', 'Ankle Lateral Force', ...
-       'Location', 'best', 'Color', 'w', 'TextColor', 'k', 'Box', 'on');
 
-set(fig2, 'InvertHardcopy', 'off');
-fprintf("Graphs Plotted!\n");
+% Left Knee Vertical Load
+subplot(2,2,4);
+set(gca, 'XColor', 'k', 'YColor', 'k');
+plot(time, abs(data_L.Force_Knee_ProximoDistalForce), 'Color', '#77AC30'); 
+title('Left Knee Vertical Load', 'Color', 'k');
+ylabel('Force (N)', 'Color', 'k'); 
+xlabel('Time (s)', 'Color', 'k'); 
+grid on;
+
+set(fig2, 'InvertHardcopy', 'off'); 
+
+%% FIGURE 3: ANKLE JOINT ANALYSIS
+fig3 = figure('Name', 'Ankle Joint Analysis', 'Color', 'w', 'Position', [150 200 1200 800]);
+
+% Right Ankle Torque
+subplot(3,2,1);
+set(gca, 'XColor', 'k', 'YColor', 'k');
+plot(time, data_R.Moment_AnklePlantarFlexion, 'Color', '#A2142F');
+title('Right Ankle Torque', 'Color', 'k');
+ylabel('Torque (Nm)', 'Color', 'k'); 
+grid on;
+
+% Left Ankle Torque
+subplot(3,2,2);
+set(gca, 'XColor', 'k', 'YColor', 'k');
+plot(time, data_L.Moment_AnklePlantarFlexion, 'Color', '#A2142F');
+title('Left Ankle Torque', 'Color', 'k');
+ylabel('Torque (Nm)', 'Color', 'k'); 
+grid on;
+
+% Right Ankle Vertical Load
+subplot(3,2,3);
+set(gca, 'XColor', 'k', 'YColor', 'k');
+plot(time, abs(data_R.Force_Ankle_ProximoDistalForce), 'Color', '#A2142F'); 
+title('Right Ankle Vertical Load', 'Color', 'k');
+ylabel('Force (N)', 'Color', 'k'); 
+grid on;
+
+% Left Ankle Vertical Load
+subplot(3,2,4);
+set(gca, 'XColor', 'k', 'YColor', 'k');
+plot(time, abs(data_L.Force_Ankle_ProximoDistalForce), 'Color', '#A2142F'); 
+title('Left Ankle Vertical Load', 'Color', 'k');
+ylabel('Force (N)', 'Color', 'k'); 
+grid on;
+
+% Right Ankle Lateral Force
+subplot(3,2,5);
+set(gca, 'XColor', 'k', 'YColor', 'k');
+plot(time, data_R.Force_Ankle_MedioLateralForce, 'Color', '#7E2F8E'); 
+title('Right Ankle Lateral Force', 'Color', 'k');
+ylabel('Force (N)', 'Color', 'k'); 
+xlabel('Time (s)', 'Color', 'k'); 
+grid on;
+
+% Left Ankle Lateral Force
+subplot(3,2,6);
+set(gca, 'XColor', 'k', 'YColor', 'k');
+plot(time, data_L.Force_Ankle_MedioLateralForce, 'Color', '#7E2F8E'); 
+title('Left Ankle Lateral Force', 'Color', 'k');
+ylabel('Force (N)', 'Color', 'k'); 
+xlabel('Time (s)', 'Color', 'k'); 
+grid on;
+
+set(fig3, 'InvertHardcopy', 'off');
+
+fprintf("Graphs Plotted!\n")
--- run_analysis.m
+++ run_analysis.m
@@ -1,11 +1,14 @@
 %% Initialization and Configuration
+clear; 
+clc;
 % Setup file paths
 inversefile = 'GRF_FullBody_IC_walker_WL_helper_ground_walk_w_rod_03_InverseDynamicStudy.anydata.h5';
 markerfile = 'GRF_FullBody_IC_walker_WL_helper_ground_walk_w_rod_03_MarkerTracking.anydata.h5';
-output_csv = 'interaction_joint_data.csv';
+output_csv_right = 'interaction_joint_data_right.csv';
+output_csv_left  = 'interaction_joint_data_left.csv';
 
 % Setup folders to extract
-base_path = '/Output/_Main/HumanModel/BodyModel/SelectedOutput/Right/Leg/';
+sides = {'Right', 'Left'};
 folders_to_scan = {'JointMomentMeasure', 'JointReactionForce'};
 target_joints = {'Hip', 'Knee', 'Ankle'};
 
@@ -19,78 +22,79 @@ else
 end
 
 %% Get time vector
-try
-    time_data = h5read(inversefile, '/Output/Abscissa/t');
-    % Initialize the results table with Time
-    ResultsTable = table(time_data, 'VariableNames', {'Time'});
-    disp(['Time vector loaded (' num2str(length(time_data)) ' frames)']);
-catch
-    error('Could not find Time vector at /Output/Abscissa/t');
-end
-
-%% Looping through folders
-for f = 1:length(folders_to_scan)
-    current_folder = [base_path, folders_to_scan{f}];
-    
+possible_time_paths = {'/Output/Abscissa/t', '/Output/t', '/Output/Model/t'};
+time_data = [];
+time_path_found = '';
+for i = 1:length(possible_time_paths)
     try
-        info = h5info(inversefile, current_folder);
-
-        % Loop through each Dataset
-        for i = 1:length(info.Datasets)
-            var_name = info.Datasets(i).Name;
-            % Check if this variable name contains Hip, Knee, or Ankle
-            is_relevant = false;
-            for k = 1:length(target_joints)
-                if contains(var_name, target_joints{k}, 'IgnoreCase', true)
-                    is_relevant = true;
-                    break;
-                end
-            end
-            %Grab target data
-            if is_relevant
-                full_path = [current_folder, '/', var_name];
-                data = h5read(inversefile, full_path);
-
-                if contains(folders_to_scan{f}, 'Moment')
-                    col_name = ['Moment_', var_name];
-                else
-                    col_name = ['Force_', var_name];
-                end
-                col_name = matlab.lang.makeValidName(col_name);
-                
-                % Add to Table
-                if length(data) == length(time_data)
-                    ResultsTable.(col_name) = data;
-                    fprintf('   Saved: %s\n', col_name);
-                else
-                    fprintf('Skipping %s: Size mismatch (%d vs %d)\n', ...
-                        col_name, length(data), length(time_data));
-                end
-            end
-        end
-        
-    catch ME
-        fprintf('Could not process folder: %s\n', current_folder);
-        disp(ME.message);
+        time_data = h5read(inversefile, possible_time_paths{i});
+        time_path_found = possible_time_paths{i};
+        fprintf('Found Time vector at: %s (%d frames)\n', time_path_found, length(time_data));
+        break; 
+    catch
     end
 end
 
-%% Export the table to a .csv file
-fprintf('Writing to %s ...\n', output_csv);
-writetable(ResultsTable, output_csv);
-fprintf('Done writing!\n');
-
-%% Code for testing (currently not in use but might be helpful)
-%{
-%% Display internal file structures
-%h5disp(inversefile, '/', 'min');
-
-%% Load the Files into Matlab
-folder_map = h5info(inversefile);
-
-%% Search for data
-for i = 1:length(folder_map.Groups)
-    find_force_data(folder_map.Groups(i), '');
+%% Looping through Sides (Right and Left)
+for s = 1:length(sides)
+    current_side = sides{s};
+    fprintf('\n--- Processing %s Leg ---\n', current_side);
+    % Base path
+    base_path = sprintf('/Output/_Main/HumanModel/BodyModel/SelectedOutput/%s/Leg/', current_side);    
+    % Reinitialize result table
+    ResultsTable = table(time_data, 'VariableNames', {'Time'});
+    % Determine which CSV to save to
+    if strcmp(current_side, 'Right')
+        current_output_csv = output_csv_right;
+    else
+        current_output_csv = output_csv_left;
+    end
+    % Looping through folders (Moments and Forces)
+    for f = 1:length(folders_to_scan)
+        current_folder = [base_path, folders_to_scan{f}];
+        try
+            info = h5info(inversefile, current_folder);  
+            % Loop through datasets
+            for i = 1:length(info.Datasets)
+                var_name = info.Datasets(i).Name;
+                % Check if this variable name contains Hip, Knee, or Ankle
+                is_relevant = false;
+                for k = 1:length(target_joints)
+                    if contains(var_name, target_joints{k}, 'IgnoreCase', true)
+                        is_relevant = true;
+                        break;
+                    end
+                end
+                % Grab target data
+                if is_relevant
+                    full_path = [current_folder, '/', var_name];
+                    data = h5read(inversefile, full_path);
+                    if contains(folders_to_scan{f}, 'Moment')
+                        col_name = ['Moment_', var_name];
+                    else
+                        col_name = ['Force_', var_name];
+                    end
+                    col_name = matlab.lang.makeValidName(col_name);
+                    % Add to table
+                    if length(data) == length(time_data)
+                        ResultsTable.(col_name) = data;
+                        fprintf('   Saved: %s\n', col_name);
+                    else
+                        fprintf('Skipping %s: Size mismatch (%d vs %d)\n', ...
+                            col_name, length(data), length(time_data));
+                    end
+                end
+            end
+        catch ME
+            fprintf('Could not process folder: %s\n', current_folder);
+            fprintf('   Error: %s\n', ME.message);
+        end
+    end
+    
+    %% Export the table to the respective .csv file
+    fprintf('Writing %s leg data to %s ...\n', current_side, current_output_csv);
+    writetable(ResultsTable, current_output_csv);
+    fprintf('Done writing %s leg!\n', current_side);
 end
-fprintf("Search Complete!\n");
-%}
+
+fprintf('\nAll Data Extraction Complete!\n');
